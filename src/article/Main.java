7366df911faa53f75d9efa8836a12f546ef71ec2
devCJH <http-@naver.com>
2023-04-03 21:46:02 +0900

    게시물 작성 기능 구현. 게시물 목록 기능 구현

--- src/article/Main.java
+++ src/article/Main.java
@@ -1,5 +1,7 @@
 package article;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,33 +12,69 @@ public class Main {
 		
 		int lastArticleId = 0;
 		
+		List<Article> articles = new ArrayList<>();
+		
 		while(true) {
 			System.out.printf("명령어) ");
-			String cmd = sc.nextLine();
+			String cmd = sc.nextLine().trim();
+			
+			if(cmd.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
 			
 			if(cmd.equals("exit")) {
-				
 				break;
+			}
+			
+			if (cmd.equals("article list")) {
 				
-			} else if (cmd.equals("article list")) {
+				if(articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+					continue;
+				}
 				
-				System.out.println("게시글이 없습니다");
+				System.out.println("번호	|	제목	");
+				
+				for(int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d	|	%s	\n", article.id, article.title);
+				}
 				
 			} else if (cmd.equals("article write")) {
-				lastArticleId++;
+				int id = lastArticleId + 1;
+				lastArticleId = id;
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 				
-				System.out.printf("%d번글이 생성되었습니다\n", lastArticleId);
+				Article article = new Article(id, title, body);
+				
+				articles.add(article);
+				
+				System.out.printf("%d번글이 생성되었습니다\n", id);
 			} else {
 				System.out.println("존재하지 않는 명령어입니다");
 			}
+			
 		}
 		
 		sc.close();
 		
 		System.out.println("== 프로그램 끝 ==");
 	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+	
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+	
 }
