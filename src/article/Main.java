138143a9581ddd2a26e8b6fb417f1bb7502b5067
devCJH <http-@naver.com>
2023-04-04 21:36:46 +0900

    게시물 작성시 현재 날짜와 시간데이터 저장

--- src/article/Main.java
+++ src/article/Main.java
@@ -28,18 +28,21 @@ public class Main {
 			}
 			
 			if (cmd.equals("article write")) {
+				
 				int id = lastArticleId + 1;
 				lastArticleId = id;
+				String regDate = Util.getNowDateStr();
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 				
-				Article article = new Article(id, title, body);
+				Article article = new Article(id, regDate, title, body);
 				
 				articles.add(article);
 				
 				System.out.printf("%d번글이 생성되었습니다\n", id);
+				
 			} else if (cmd.equals("article list")) {
 				
 				if(articles.size() == 0) {
@@ -47,11 +50,11 @@ public class Main {
 					continue;
 				}
 				
-				System.out.println("번호	|	제목	");
+				System.out.println("번호	|	제목	|	날짜	");
 				
 				for(int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
-					System.out.printf("%d	|	%s	\n", article.id, article.title);
+					System.out.printf("%d	|	%s	|	%s	\n", article.id, article.title, article.regDate);
 				}
 				
 			} else if (cmd.startsWith("article detail ")) {
@@ -77,6 +80,7 @@ public class Main {
 				
 				System.out.println("== 게시글 상세보기 ==");
 				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
 				
@@ -119,11 +123,13 @@ public class Main {
 
 class Article {
 	int id;
+	String regDate;
 	String title;
 	String body;
 	
-	public Article(int id, String title, String body) {
+	public Article(int id, String regDate, String title, String body) {
 		this.id = id;
+		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
 	}
---
+++ src/article/Util.java
@@ -0,0 +1,12 @@
+package article;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+    public static String getNowDateStr() {
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date now = new Date();
+        return sdf1.format(now);
+    }
+}
